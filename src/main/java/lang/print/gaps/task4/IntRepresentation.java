d5baa62189cb23d23b1c84f0dc65c09c22cd0853
Maksim U <20mingot@gmail.com>
2023-06-02 21:03:12 +0600

    add task 4

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c,h,a,r;
+        c =99;
+        h=104;
+        a=97;
+        r=114;
+        System.out.println(c+"\n"+h+"\n"+a+"\n"+r);
     }
 }
